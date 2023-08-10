e52c3e31a2f53c3e765b2a256a426fcfd4cfed56
umekishimaru <riki12344649@gmail.com>
2023-08-10 16:22:05 +0900

    rails server起動成功

--- game.rb
+++ game.rb
@@ -4,35 +4,44 @@
 =end 
 
 
+class Strack_out
+
+  def initialize(height,width)
+    @height = height
+    @width = width
+    @strack_panel_num = width * height 
+
+    @strack_panel_num_array = []
+    @strack_panel_num.times do 
+      @number = rand(1..100)
+      @maru_batsu_string = ["○","✖️"].sample
+     @hash = {@maru_batsu_string => @number}
+     @strack_panel_num_array.push(hash)
+    end
+    @answer = 0
+  end
+
+  def answer
+    @maru_numbers = []
+    @strack_panel_num_array.each do |hash|
+      hash_key = hash.keys
+      @answer += hash["○"] if hash_key == ["○"]
+  end
+
+
+    
+ 
+end
+puts answer 
+
+
+end 
+
+puts 高さを入力してください
 height = gets.to_i
 
+puts 幅を入力してください
 width = gets.to_i
 
-strack_panel_num = width * height 
-
-
-
-strack_panel_num_array = []
-
-strack_panel_num.times do 
-  number = gets.to_i
-  maru_batsu_string = ["○","✖️"].sample
- hash = {maru_batsu_string => number}
- strack_panel_num_array.push(hash)
-end
-
-maru_numbers = []
-strack_panel_num_array.each do |hash|
-  hash_key = hash.keys.to_s
-
-
-  if hash_key == "○"
-  puts "丸"
-  end
-    
-  #  value =  hash[hash_key.to_s]
-  #  maru_numbers.push(value)
-  # end
-end
-
+strack_out = Strack_out.new(height,width)
 # puts maru_numbers
