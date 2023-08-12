bcc3b8da85422e584817dc569cd3cdf67f87f7c7
umekishimaru <riki12344649@gmail.com>
2023-08-12 14:06:10 +0900

    modal_rspec完成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
 
-  
-it "A user creates an account" do 
-  visit  users_home_path
+ describe "user creates an account" do 
+    it "アカウント新規作成したらDBが+1になる" do 
+      visit  users_home_path
+      click_on 'アカウント新規登録へ'
+
+      fill_in 'recipient-name',     with: 'まさなかにき'
+      fill_in 'recipient-email',    with: 'amgajdjkd@yahoo.co.jp'
+      fill_in 'recipient-password', with: 'box1234'
+      click_on '登録'
 
 
-end
-
-end 
+    end
+   end 
+  end 
