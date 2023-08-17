2a6cd2546f9f659bcdc1f8c657dfe842014313f5
umekishimaru <riki12344649@gmail.com>
2023-08-17 16:02:09 +0900

    rspec ユーザーバリデーションテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name                {"梅田"}
-    email               {"amagkd@yahoo.co.jp"}
-    password            {"masamasa4649"}
+    name                    {"梅田"}
+    email                   {"amagkd@yahoo.co.jp"}
+    password                {"masamasa4649"}
+    password_confirmation   {"masamasa4649"}
   end
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do 
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'nameカラム' do 
+    it 'is invalid without a name' do 
+     @user.name = nil
+     expect(@user).to be_invalid
+    end   
+
+    it 'should be unique in name' do 
+      @user.save
+      user = FactoryBot.build(:user, name: '梅田')
+      expect(user).to be_invalid
+    end
+  end 
 end
