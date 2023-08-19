0f55d776a482a08309de8376b829c834feaee509
umekishimaru <riki12344649@gmail.com>
2023-08-19 17:24:06 +0900

    アカウント登録画面完成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   get 'users/home'
   get 'users/new'
-  post 'users/create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
