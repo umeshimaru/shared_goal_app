a36aafa608fe99fac544c2e196392a9c35c17b3e
umekishimaru <riki12344649@gmail.com>
2023-08-30 16:05:41 +0900

    確認メール後のルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,21 @@ Rails.application.routes.draw do
   resources :monthly_goals
   devise_for :users,  controllers: {
     registrations:  'users/registrations',
-    confirmations:  'users/confirmations'
+    confirmations:  'users/confirmations',
+   
+
   }
   devise_scope :user do
     get '/confirmation_email', to: 'users/confirmations#check_email'
+    get '/monthly_goals/new',  to: 'users/monthly_goals#new'
+    
   end
 
   get 'users/home'
   get 'users/new'
-  get '/my_page/:id',to: 'users#my_page', as: 'my_page'
+  get '/my_page/:id',       to: 'users#my_page', as: 'my_page'
+
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
