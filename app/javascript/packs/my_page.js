42f496e3474102de123aac24f2a6036f9a1a30c4
umekishimaru <riki12344649@gmail.com>
2023-09-04 17:57:46 +0900

    jqueryでパラメーターのid取得

--- app/javascript/packs/my_page.js
+++ app/javascript/packs/my_page.js
@@ -14,21 +14,51 @@
                 return;
         } else {
                   const button = $("<button>", {
-                  text: "更新",
+                  text:  '更新',
                   class: 'monthly_goal_update'
+      
                   })
                   $('.monthly_goal').append(button);
+                
                 };
-    
-                // 2回目の編集リンククリック時の処理
+
+                $('.monthly_goal_update').on('click', (e) => {
+                  e.preventDefault();
+                  const idNumber = 2;
+                  let currentURL = location.href;
+                  let path = new URL(currentURL).pathname;
+                  let path_array = path.split('/');
+                  let id = path_array[idNumber]
+                  let inputValue = $('input').val();
+
+                  $.ajax({
+                    type: 'PATCH', 
+                    url: `monthly_goals/${id}`, 
+                    data: { // サーバーへ送信するデータ
+                           monthly_goal: inputValue
+                          }
+
+                  
+                
+                });
+
+
+              
+
+
+                // })
+
+              
+                // 2回目の編集リンククリック時の処理 ①インプット要素をpタグに戻す。更新ボタンの削除
     $(document).on('click', function(e) {
       if (!$(e.target).is('.monthly_goal_update,.edit-button,input')) {
-        const updatedText = input.val();
-        const newParagraph = $("<p>").text(updatedText).addClass("change_goal");
-        input.replaceWith(newParagraph);
+
+        let monthly_goal = $("<p>").text(change_goal).addClass("change_goal");
+        input.replaceWith(monthly_goal);
         $('.monthly_goal_update').remove();
       }
     });
+
   });
     
 
