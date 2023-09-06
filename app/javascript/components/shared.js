593324e687f7fbb8c7bb0f914f12b664daf9e141
umekishimaru <riki12344649@gmail.com>
2023-09-06 18:35:07 +0900

    my_goal.htmlのペナルティの欄に編集機能追加

--- app/javascript/components/shared.js
+++ app/javascript/components/shared.js
@@ -22,23 +22,30 @@ export function changeMonthlyGoal(){
 
 
 
- export function checkAndCreateButton(){
-                                        if ($(".monthly_goal_update").length) {
+ export function checkAndCreateButton(updateButton,cssClass,section){
+                                        if ($(updateButton).length) {
                                           return;
                                         } else {
                                           const button = $("<button>", {
                                           text:  '更新',
-                                          class: 'monthly_goal_update'
+                                          class: cssClass
 
                                           })
-                                          $('.monthly_goal').append(button);
+                                          $(section).append(button);
     
                                         };
                                         }
 
  export function cancelEdit(e,change_goal,input){
-                              if (!$(e.target).is('.monthly_goal_update,.edit-button,input')) {
-                              let monthly_goal = $("<p>").text(change_goal).addClass("change_goal");
-                              input.replaceWith(monthly_goal);
-                              $('.monthly_goal_update').remove();
-                              };}
+                                                  if (!$(e.target).is('.monthly_goal_update,.edit-button,input')) {
+                                                  let monthly_goal = $("<p>").text(change_goal).addClass("change_goal");
+                                                  input.replaceWith(monthly_goal);
+                                                  $('.monthly_goal_update').remove();
+                                                 };}
+
+ export function cancelPenaltyEdit(e,change_panalty_name,penalty_input){
+                                                  if (!$(e.target).is('.penalty_name_update,.penalty_edit_button,penalty_input')) {
+                                                  let penalty_name = $("<p>").text(change_panalty_name).addClass("change_penalty_name");
+                                                  penalty_input.replaceWith(penalty_name);
+                                                  $('.penalty_name_update').remove();
+                                                 };}
--- app/javascript/packs/my_page.js
+++ app/javascript/packs/my_page.js
@@ -1,13 +1,15 @@
 import { changeMonthlyGoal }   from "../components/shared"; 
 import { checkAndCreateButton} from "../components/shared"; 
 import { cancelEdit}           from "../components/shared"; 
+import { cancelPenaltyEdit}    from "../components/shared"; 
+
   $(document).on('turbolinks:load', function() {
     $('.edit-button').on('click', (e) => {
                                            e.preventDefault(); 
                                            let change_goal = $(".change_goal").text();
                                            let input = $("<input>").val($(".change_goal").text());
                                            $('.change_goal').replaceWith(input);
-                                           checkAndCreateButton();
+                                           checkAndCreateButton(".monthly_goal_update",'monthly_goal_update','.monthly_goal');
 
                                            $('.monthly_goal_update').on('click', (e) => {
                                                                                          e.preventDefault();
@@ -16,17 +18,27 @@ import { cancelEdit}           from "../components/shared";
                                            $(document).on('click', function(e) {
                                                                                 cancelEdit(e,change_goal,input);
     });
-
   });
-    
+    $('.penalty_edit_button').on('click', (e) => {
+      e.preventDefault(); 
+      let change_panalty_name = $(".change_penalty_name").text();
+      let penalty_input = $("<input>").val($(".change_penalty_name").text());
+      $('.change_penalty_name').replaceWith(penalty_input);
+      checkAndCreateButton(".penalty_name_update",'penalty_name_update','.penalty');
+      $(document).on('click', function(e) {
+       cancelPenaltyEdit(e,change_panalty_name,penalty_input);
 
-    // $('.edit-button').on('click', () => {
-
-    // });
-
-
+       
+       
      
+
+
+
+
 });
+  });
+});
+  
 // let element = $(e.target);
 // if (!element.is(".monthly_goal_update")) {
 //   $("monthly_goal_update").remove();
