70b1566c3cdd16b410bf21cb7e4b0756c1cc3e99
umekishimaru <riki12344649@gmail.com>
2023-09-26 14:01:16 +0900

    UserテーブルとWeekly _goalテーブルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_one :monthly_goal
+  has_many :weekly_goals, through: :monthly_goal
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
