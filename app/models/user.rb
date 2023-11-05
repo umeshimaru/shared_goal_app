b785c1807b8f34f5593db8c1d6438d688e3e8fe2
umekishimaru <riki12344649@gmail.com>
2023-11-05 22:43:58 +0900

    notificationモデル作成と関連付け作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
                                     foreign_key: "reciever_id",
                                     dependent:   :destroy
 
+  has_many :active_notifications,  class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
 
                            
