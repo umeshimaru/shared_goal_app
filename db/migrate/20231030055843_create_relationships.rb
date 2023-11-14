715c6d5f6eae092bddf837184f5ee794021563c7
umekishimaru <riki12344649@gmail.com>
2023-11-14 23:15:21 +0900

    1人のユーザーにつき１人にしか友達申請送れないし受け友達申請送れないし受け取れない

--- db/migrate/20231030055843_create_relationships.rb
+++ db/migrate/20231030055843_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-      t.integer :sender_id
-      t.integer :reciever_id
+      t.integer :sender_id ,  unique: true
+      t.integer :reciever_id, unique: true
 
       t.timestamps
     end
